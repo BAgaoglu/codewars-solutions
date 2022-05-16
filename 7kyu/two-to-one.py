49de8a47baa2d8d61806f66404706e0ace5fa8a1
Beyza Ağaoğlu <89702986+BAgaoglu@users.noreply.github.com>
2022-05-16 17:46:26 +0300

    added the solution for two to one

---
+++ 7kyu/two-to-one.py
@@ -0,0 +1,22 @@
+# Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+# Examples:
+# a = "xyaabbbccccdefww"
+# b = "xxxxyyyyabklmopq"
+# longest(a, b) -> "abcdefklmopqwxy"
+
+# a = "abcdefghijklmnopqrstuvwxyz"
+# longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+# My Solution
+
+def longest(a1, a2):
+    
+    result=""
+    #concatenate string
+    conString = a1+a2
+    
+    for x in sorted(set(conString)):
+        result += x
+        
+    return result
