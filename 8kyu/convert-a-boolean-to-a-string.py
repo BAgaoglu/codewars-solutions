72f05b6e063ae7baabb22bb70ae48dc1dfe8de7a
Beyza Ağaoğlu <89702986+BAgaoglu@users.noreply.github.com>
2022-05-16 17:42:45 +0300

    added the solution

---
+++ 8kyu/convert-a-boolean-to-a-string.py
@@ -0,0 +1,14 @@
+# Convert a Boolean to a String
+
+
+# Implement a function which convert the given boolean value into its string representation.
+# Note: Only valid inputs will be given.
+
+
+# My solution
+
+def boolean_to_string(b):
+    new = str(b)
+    return new
+
+
